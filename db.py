e7b4a489e0736395d3998a5e67ff44434b1f86e9
alex01 <alextobar013@gmail.com>
2026-05-22 16:54:12 +0200

    cambio3

--- db.py
+++ db.py
@@ -6,7 +6,14 @@ class DB:
     def __init__(self):
         self.dsn = os.getenv("DATABASE_URL")
         if not self.dsn:
-            raise ValueError("DATABASE_URL no está configurada en Dokploy")
+            raise ValueError("DATABASE_URL no configurada")
+        
+        # Asegurar SSL si no viene en la URL
+        if "sslmode" not in self.dsn:
+            if "?" in self.dsn:
+                self.dsn += "&sslmode=require"
+            else:
+                self.dsn += "?sslmode=require"
 
     def connect(self):
         return psycopg2.connect(self.dsn, cursor_factory=RealDictCursor)
